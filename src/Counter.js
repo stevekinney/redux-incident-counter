503acc18b050b85f17c6f6350fb804d20f25787e
Steve Kinney <hello@stevekinney.net>
2021-05-02 06:34:30 -0600

    Add useCounter

--- src/Counter.js
+++ src/Counter.js
@@ -1,23 +1,18 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { decrement, increment, set } from './actions';
 import { SetCounter } from './SetCounter';
+import { useCounter } from './use-counter';
 
 export const Counter = () => {
   const incident = 'Incident';
-  const count = useSelector((state) => state.count);
-  const dispatch = useDispatch();
-
-  const actions = bindActionCreators({ increment, decrement, set }, dispatch);
+  const { count, increment, decrement, set } = useCounter();
 
   return (
     <main className="Counter">
       <h1>Days Since Last {incident}</h1>
       <p className="count">{count}</p>
       <section className="controls">
-        <button onClick={() => actions.increment()}>Increment</button>
-        <button onClick={() => actions.set(0)}>Reset</button>
-        <button onClick={() => actions.decrement()}>Decrement</button>
+        <button onClick={() => increment()}>Increment</button>
+        <button onClick={() => set(0)}>Reset</button>
+        <button onClick={() => decrement()}>Decrement</button>
       </section>
       <SetCounter />
     </main>
