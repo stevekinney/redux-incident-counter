09f287f71b1261ebba4833e573e8b64a387b4fc3
Steve Kinney <hello@stevekinney.net>
2021-05-01 20:35:50 -0600

    Add fake props

--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,11 @@
 export const Counter = () => {
+  const incident = 'Incident';
+  const count = 0;
+
   return (
     <main className="Counter">
-      <h1>Days Since Last Incident</h1>
-      <p className="count">0</p>
+      <h1>Days Since Last {incident}</h1>
+      <p className="count">{count}</p>
       <section className="controls">
         <button>Increment</button>
         <button>Reset</button>
