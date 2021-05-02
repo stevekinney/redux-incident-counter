0487546b17a3df7a9810efac39d04701b05ea910
Steve Kinney <hello@stevekinney.net>
2021-05-02 05:02:36 -0600

    Add store to React application

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
+
 import Application from './Application';
+import { store } from './store';
 
 import './index.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Application />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Application />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
