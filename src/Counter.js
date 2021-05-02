6569394029423e503a03f1f3caebebbb5a8938a8
Steve Kinney <hello@stevekinney.net>
2021-05-02 06:05:20 -0600

    Bind actions to dispatch

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { decrement, increment, set } from './actions';
 import { SetCounter } from './SetCounter';
 
@@ -7,14 +8,16 @@ export const Counter = () => {
   const count = useSelector((state) => state.count);
   const dispatch = useDispatch();
 
+  const actions = bindActionCreators({ increment, decrement, set }, dispatch);
+
   return (
     <main className="Counter">
       <h1>Days Since Last {incident}</h1>
       <p className="count">{count}</p>
       <section className="controls">
-        <button onClick={() => dispatch(increment())}>Increment</button>
-        <button onClick={() => dispatch(set(0))}>Reset</button>
-        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <button onClick={() => actions.increment()}>Increment</button>
+        <button onClick={() => actions.set(0)}>Reset</button>
+        <button onClick={() => actions.decrement()}>Decrement</button>
       </section>
       <SetCounter />
     </main>
