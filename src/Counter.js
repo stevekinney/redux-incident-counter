45f1a4112ae3ae660001bbce15f6621a599c4f77
Rajat Bansal <rjtbansal@gmail.com>
2021-07-21 21:13:21 -0400

    hooked up buttons with our actions

--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, set } from './actions';
+
 export const Counter = () => {
   const incident = 'Incident';
-  const count = 0;
-
+  /**A hook to access the redux store's state. This hook takes a selector function as an argument. The selector is called with the store state. */
+  //useSelector replacement for mapStateToProps
+  const count = useSelector((state) => state.count);
+  //useDispatch is a replacement for mapDispatchToProps and it gives us access to redux dispatch method
+  const dispatch = useDispatch();
   return (
     <main className="Counter">
       <h1>Days Since Last {incident}</h1>
       <p className="count">{count}</p>
       <section className="controls">
-        <button>Increment</button>
-        <button>Reset</button>
-        <button>Decrement</button>
+        {/* dispatching our actions below. set is the only one that takes an argument */}
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(set(0))}>Reset</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
       </section>
     </main>
   );
