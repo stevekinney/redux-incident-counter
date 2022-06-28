affdf7d5496e1b554297f2fab6a015522c15f6f9
Shawn DI <ShawnsuDI@Shawns-MBP13-2018.local>
2022-06-28 18:42:55 +1200

    chore: delete reset action

--- src/actions.js
+++ src/actions.js
@@ -1,11 +1,9 @@
 const DECREMENT = 'DECREMENT';
 const INCREMENT = 'INCREMENT';
-const RESET = 'RESET';
 const SET = 'SET';
 
 const increment = () => ({ type: INCREMENT });
 const decrement = () => ({ type: DECREMENT });
-const reset = () => ({ type: RESET, payload: 0 });
 const set = (val) => ({ type: SET, payload: val });
 
-export { DECREMENT, INCREMENT, RESET, increment, decrement, reset, set };
+export { DECREMENT, INCREMENT, SET, increment, decrement, set };
